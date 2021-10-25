7753bcf6a40646b0ca9fe1561c998453397823a9
Johnny Seay <seay.johnny@gmail.com>
2021-10-25 01:37:02 -0500

    Fixed relative paths in script.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,8 @@
 $(function() {
 
-    let base;
-    if(window.location.host == "127.0.0.1:3000") {
-        base = ""
-    } else {
-        base = "fish-care-guide/"
-    }
-    console.log(base)
-
     var includes = $('[data-include]')
     $.each(includes, function() {
-        var file = base + 'templates/' + $(this).data('include') + '.html'
+        var file = 'templates/' + $(this).data('include') + '.html'
         $(this).load(file)
     })
 
@@ -18,7 +10,7 @@ $(function() {
         var route = $(this).data("route")
         let loc
         if (route) {
-            loc = base + route + '.html'
+            loc = route + '.html'
         } else {
             loc = '#'
         }
@@ -29,7 +21,7 @@ $(function() {
     $(".nc > .nc-nav > a").click(function() {
         var route = $(this).data("content")
         if (route) {
-            var file = base + 'templates/' + route + '.html'
+            var file = 'templates/' + route + '.html'
             $(".nc .nc-content").load(file)
         } 
         window.location = loc
